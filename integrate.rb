acbe61e0a67af8a950d311e681e600ff7b7b5700
Benjamin Kampmann <ben.kampmann@gmail.com>
2014-03-31 12:38:47 +0200

    register new archetype so it shows up in lists

--- integrate.rb
+++ integrate.rb
@@ -1,1 +1,11 @@
 
+class QuestionArchetype < Archetype
+end
+
+
+arch = QuestionArchetype.new("question", {
+    capabilities: [:searchable, :shown_publicly, :linkable]})
+
+
+puts arch.options
+Archetype.register("question", { archetype: arch })
