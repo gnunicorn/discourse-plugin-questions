d70e410cb124d65e152cd6c229660b95a8249ed5
Benjamin Kampmann <ben.kampmann@gmail.com>
2014-03-31 16:57:06 +0200

    for questions we want to resort on likes

--- integrate.rb
+++ integrate.rb
@@ -1,11 +1,35 @@
 
+QUESTION_ARCHETYPE_NAME = "question"
+
+module QuestionsLikeResorter
+    def self.included(klass)
+        klass.after_save :resort_for_likes
+    end
+
+    def resort_for_likes
+        return unless is_like?
+        return unless post.topic.archetype == QUESTION_ARCHETYPE_NAME
+        posts = Post.where(topic_id: post.topic_id).where("post_number != 1").order("like_score desc")
+        # the first one shall always be on top, so we add + 1 to all other posts
+        Post.transaction do
+            posts.each_with_index do |post, index|
+                post.sort_order = index + 1
+                post.save()
+            end
+        end
+    end
+end
+
+PostAction.send :include, QuestionsLikeResorter
+
+
+## Create and Register new Archetype
+
 class QuestionArchetype < Archetype
 end
 
-
-arch = QuestionArchetype.new("question", {
+arch = QuestionArchetype.new(QUESTION_ARCHETYPE_NAME, {
     capabilities: [:searchable, :shown_publicly, :linkable]})
 
 
-puts arch.options
-Archetype.register("question", { archetype: arch })
+Archetype.register(QUESTION_ARCHETYPE_NAME, { archetype: arch })
