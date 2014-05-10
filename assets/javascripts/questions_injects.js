12b369b8f32d22d3e71711a45ea20e32dc8cc6a4
Benjamin Kampmann <ben.kampmann@gmail.com>
2014-05-10 18:34:47 +0200

    render the path properly

--- assets/javascripts/questions_injects.js
+++ assets/javascripts/questions_injects.js
@@ -4,7 +4,7 @@
     // patcher.setDebug(true);
     // patcher.setFinder('composerMessages');
 
-    var patched = '{{#if model.archetypeSelectable}}{{i18n composer.create}}<label>{{view Discourse.QRadioButton name="archetypeSelection" selectionBinding="model.archetypeId" value="regular"}}{{i18n composer.regular_topic}}</label> <label>{{view Discourse.QRadioButton name="archetypeSelection" selectionBinding="model.archetypeId" value="question"}}{{i18n composer.question}}</label>{{else}}{{model.actionTitle}}{{/if}}'
+    var patched = '{{#if model.archetypeSelectable}}{{i18n composer.create}}<label>{{view Discourse.QRadioButton name="archetypeSelection" selectionBinding="model.archetypeId" value="regular"}}{{i18n composer.regular_topic}}</label> <label>{{view Discourse.QRadioButton name="archetypeSelection" selectionBinding="model.archetypeId" value="question"}}{{i18n composer.question}}</label>{{else}}{{{model.actionTitle}}}{{/if}}'
 
     //wrap outlet into our section, attach aside
     patcher.addGeneralPatcher("f5a96e884170ace97506d3e4ca3eb3d3",
