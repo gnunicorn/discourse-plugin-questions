0f50c73718b93ce6c68b6dad23b4724626bbe773
Benjamin Kampmann <ben.kampmann@gmail.com>
2014-05-12 11:58:26 +0200

    moving it into its own file prevents the ugly parsing error

--- plugin.rb
+++ plugin.rb
@@ -12,39 +12,7 @@ register_asset "javascripts/questions/questions_post_view.js"
 register_asset "javascripts/questions/questions_creations_view.js"
 register_asset "javascripts/discourse/templates/big_like.js.handlebars"
 
-register_css <<CSS
-
-.bigLikeNumber {
- text-align: center;
-}
-
-.bigLikeNumber:before {
-    display: block;
-    position: absolute;
-    top: 5px;
-    left: 5px;
-    color: #F5F5F5;
-    font: 90px/1 FontAwesome;
-    content: "\f004";
-    z-index: -5;
-}
-
-.bigLikeNumber h2 {
-    color: #333;
-    font-size: 45px;
-    line-height: 90px;
-}
-
-#reply-control .control .reply-to label {
-    display: inline;
-    margin: 0 2px;
-}
-
-#reply-control .control .reply-to label input {
- margin: 0 3px;
-}
-
-CSS
+register_asset "question_styles.css"
 
 after_initialize do
 	require_dependency File.expand_path('../integrate.rb', __FILE__)
