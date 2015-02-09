e48247ff7e38b50789cd9d2c19bf75ab2cd85d86
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-02-09 17:24:39 +0000

    remove cruft

--- assets/javascripts/questions/questions_post_view.js
+++ assets/javascripts/questions/questions_post_view.js
@@ -1,12 +1,6 @@
 
 // make detection easier
 Discourse.Topic.reopen({
-    urlFillerData: function(){
-        data = this._super();
-        data.push("isQuestion");
-        return data;
-    }.property(),
-
     urlForPostNumber: function(post_number) {
         // on questions, the posts order changes and therefore the links
         // using counters are broken. Don't pretend this
